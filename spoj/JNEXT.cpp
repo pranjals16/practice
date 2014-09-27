f81b366ceeb8d9c0f4296c90a66d91603f59620f
pranjals16 <pranjals16@gmail.com>
2014-09-27 14:42:32 +0530

    27 Sep 14

--- spoj/JNEXT.cpp
+++ spoj/JNEXT.cpp
@@ -4,33 +4,43 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
+void find(int arr[],int n){
+	int j=n-1;
+	while(j>0){
+		if(arr[j-1]<arr[j])
+			break;
+		else
+			j--;
+	}
+	int temp=arr[j-1];
+	int min=arr[j]-temp;
+	int i=j,index=i;
+	while(i<n){
+		if(arr[i]>temp && (arr[i]-arr[i-1])<min){
+			min=arr[i]-arr[i-1];
+			index=i;
+		}
+		i++;
+	}
+	int temp2=arr[index];
+	arr[index]=temp;
+	arr[j-1]=temp2;
+	sort(arr+(j),arr+n);
+	for(int i=0;i<n;i++)
+		cout << arr[i];
+	cout << endl;
+}
 
 int main()
 {
-	int t;
+	int t,n;
 	cin >> t;
 	while(t--){
-		int n,m;
-		cin >> n >> m;
-		vector<int> arr(n);
+		cin >> n;
+		int arr[n];
 		for(int i=0;i<n;i++)
 			cin >> arr[i];
-		if(n==1){
-			cout << "1\n";
-			continue;
-		}
-		else{
-			int count=0;
-			while(true){
-				int max_index=max_element((arr).begin(),(arr).end())-arr.begin();
-				++count;
-				if(max_index==m) break;
-				rotate(arr.begin(),arr.begin()+max_index+1,arr.end());
-				m=(m+(int)(arr).size()-max_index-1)%(int)(arr).size();
-				arr.pop_back();
-			}
-			cout << count << endl;
-		}
+		find(arr,n);
 	}
 	return 0;
 }
